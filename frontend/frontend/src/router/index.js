53472df7801c15654f48f271e9f11adc6c245942
murilo_locatti <100702177+locattimurilo@users.noreply.github.com>
2026-05-22 11:51:54 -0300

    Update index.js

--- frontend/frontend/src/router/index.js
+++ frontend/frontend/src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "@/components/Home.vue";
 import Itens from "@/components/Items.vue";
+import Sobre from "@/components/Sobre.vue";
 
 Vue.use(VueRouter);
 
 const rotas = [
   {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
     path: "/itens",
     name: "Itens",
     component: Itens,
+  },
+  {
+    path: "/sobre",
+    name: "Sobre",
+    component: Sobre,
   },
 ];
 
